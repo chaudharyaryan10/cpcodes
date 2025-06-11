ba5ef3d774718e30ca3469f73af3f6c431419e3b
chaudharyaryan10 <aryan1411chaudhary@gmail.com>
2025-06-11 18:32:53 +0530

    Update union_find.cpp

--- graphs/dsu/union_find.cpp
+++ graphs/dsu/union_find.cpp
@@ -1,78 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-class DisjointUnionSets {
-    vector<int> rank, parent;
-
+class DisjointSet {
+    vector<int> rank, parent, size;
 public:
-  
-    // Constructor to initialize sets
-    DisjointUnionSets(int n) {
-        rank.resize(n, 0);
-        parent.resize(n);
-
-        // Initially, each element is in its own set
-        for (int i = 0; i < n; i++) {
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
             parent[i] = i;
+            size[i] = 1;
         }
     }
 
-    // Find the representative of the set that x belongs to
-    int find(int i) {
-        int root = parent[i];
-      
-        if (parent[root] != root) {
-            return parent[i] = find(root);
-        }
-      
-        return root;
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
     }
 
-    // Union of sets containing x and y
-    void unionSets(int x, int y) {
-        int xRoot = find(x);
-        int yRoot = find(y);
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
 
-        // If they are in the same set, no need to union
-        if (xRoot == yRoot) return;
-
-        // Union by rank
-        if (rank[xRoot] < rank[yRoot]) {
-            parent[xRoot] = yRoot;
-        } else if (rank[yRoot] < rank[xRoot]) {
-            parent[yRoot] = xRoot;
-        } else {
-            parent[yRoot] = xRoot;
-            rank[xRoot]++;
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
         }
     }
 };
-
 int main() {
-    // Let there be 5 persons with ids 0, 1, 2, 3, and 4
-    int n = 5;
-    DisjointUnionSets dus(n);
+    DisjointSet ds(7);
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
+    // if 3 and 7 same or not
+    if (ds.findUPar(3) == ds.findUPar(7)) {
+        cout << "Same\n";
+    }
+    else cout << "Not same\n";
 
-    // 0 is a friend of 2
-    dus.unionSets(0, 2);
+    ds.unionBySize(3, 7);
 
-    // 4 is a friend of 2
-    dus.unionSets(4, 2);
-
-    // 3 is a friend of 1
-    dus.unionSets(3, 1);
-
-    // Check if 4 is a friend of 0
-    if (dus.find(4) == dus.find(0))
-        cout << "Yes\n";
-    else
-        cout << "No\n";
-
-    // Check if 1 is a friend of 0
-    if (dus.find(1) == dus.find(0))
-        cout << "Yes\n";
-    else
-        cout << "No\n";
-
+    if (ds.findUPar(3) == ds.findUPar(7)) {
+        cout << "Same\n";
+    }
+    else cout << "Not same\n";
     return 0;
 }
