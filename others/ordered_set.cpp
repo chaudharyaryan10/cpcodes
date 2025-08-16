bc3388c82c8ef73d387fc63fc9b9d02b043c73f2
chaudharyaryan10 <aryan1411chaudhary@gmail.com>
2025-08-16 11:29:38 +0530

    Create ordered_set.cpp

---
+++ others/ordered_set.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // pbds A, *A.find_by_order(x), A.order_of_key(x),lb,upb
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+   while(t--){
+    // solve();
+    cout<<endl;
+   }
+    return 0;
+}
